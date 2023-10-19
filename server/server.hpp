12da12297633ec7006b56c425b124f8fe51c1f63
godfreykaris <godfreykarisw@gmail.com>
2023-10-19 15:58:52 +0300

    Setting up boost io_context for multiple concurrent connections

--- server/server.hpp
+++ server/server.hpp
@@ -12,18 +12,17 @@ using tcp = boost::asio::ip::tcp;
 class Server 
 {
     public:
-        Server(net::io_context& io_context, short port);
+        Server(net::io_context& io_context, short number_of_threads, short port);
         ~Server();
         void run();
-        void start();
-
-        net::io_context io_context;
-        tcp::acceptor acceptor_;
-        tcp::socket socket_;
 
     private:
         void accept();
-        void handle_request();
+        void handle_request(tcp::socket& socket_);
         void handle_get_request(const http::request<http::dynamic_body>& request, http::response<http::string_body>& response);
+
+        tcp::acceptor acceptor_;
+        net::io_context& io_context_;
+        short number_of_threads_ = 1;
 
 };
